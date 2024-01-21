a6ef2cfddce6cf8aeebf2396c54ba8d2c0e8348b
moboluwade <folarinboluwade.com>
2024-01-21 23:41:35 +0100

    add polyfill library configurations

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  entry: {
+    app: './src/app.js',
+    polyfill: './src/polyfill.js'
+  },
   plugins: [react()],
 })

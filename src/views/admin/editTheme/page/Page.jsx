5cb2e82d97062b92c1eee8c7e519681ae7b1ceb6
moboluwade <folarinboluwade.com>
2024-02-20 17:52:03 +0100

    fix: make button link to next page

--- src/views/admin/editTheme/page/Page.jsx
+++ src/views/admin/editTheme/page/Page.jsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query"
 import { useState } from "react";
 import TitleInput from "../../../../components/titleInput/TitleInput";
 import { motion } from 'framer-motion'
+import { Link } from "react-router-dom";
 // import { useNavigate } from "react-router-dom";
 
 const Page = () => {
@@ -60,19 +61,18 @@ const Page = () => {
                 {/* <h1 className={`text-4xl font-semibold w-[16rem] h-[3rem] bg-[#EBEDEF] placeholder:text-black focus:outline-none`}>bladzijde</h1> */}
             </div>
             <div className="grid grid-cols-3 gap-20">
-                <motion.button
-                    whileTap={tapCheck}
-                    whileHover={hoverCheck}
-                    className={`flex flex-col w-[16rem] h-[8rem] rounded-md bg-black text-white bg-gradient-to-br ${!nameTaken ? 'from-purple-400 to-purple-700' : 'from-gray-400 to-gray-700'} `}>
+                <Link
+                    to='/admin/thema/bladzijde/create'>
                     <motion.button
+                        whileTap={tapCheck}
+                        whileHover={hoverCheck}
                         disabled={nameTaken}
-                        // onClick={() => { console.log('theme') }}
-                        to='/admin/thema/bladzijde/create'>
+                        className={`flex flex-col w-[16rem] h-[8rem] rounded-md bg-black text-white bg-gradient-to-br ${!nameTaken ? 'from-purple-400 to-purple-700' : 'from-gray-400 to-gray-700'} `}>
                         <div className="flex flex-row">
                             <div className="inline-block p-6 text-2xl font-semibold text-center">Nieuwe pagina toevoegen <img className="inline-block w-10 h-10" src={plusImg} alt="plus icon" /></div>
                         </div>
                     </motion.button>
-                </motion.button>
+                </Link>
 
                 {data !== (undefined) && data.map((template) => {
                     const index = Math.random() * 1000
@@ -88,7 +88,7 @@ const Page = () => {
                     <img className="mx-auto h-7 w-7" src="/save-svgrepo-com.svg" alt="" />
                 </motion.button> */}
             </div>
-        </div>
+        </div >
     )
 }
 

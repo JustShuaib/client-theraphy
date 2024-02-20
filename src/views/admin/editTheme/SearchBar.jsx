05d30c9e1f9b4d2e3ba75429b34c4a3e82158775
moboluwade <folarinboluwade.com>
2024-02-20 19:26:54 +0100

    fix: make dynamic search request a json

--- src/views/admin/editTheme/SearchBar.jsx
+++ src/views/admin/editTheme/SearchBar.jsx
@@ -17,7 +17,7 @@ const SearchBar = ({ searchResult, setSearchResult, pickedSearch, setPickedSearc
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(searchValue),
+                body: JSON.stringify({ query: searchValue }),
             })
             if (!response.ok) {
                 throw new Error('Network response was not ok')
